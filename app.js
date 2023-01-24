b449868c3e3c71766a1cf7592a099779d02b6519
atulinfinite <atul.kumar2@infinite.com>
2023-01-24 16:50:41 +0530

    routers and pages created

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const http = require("http");
+const fs = require("fs");
+const { threadId } = require("worker_threads");
+const PORT = 4000;
 
 const server = http.createServer();
 
-server.on("request", (request, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World! node",
-    })
-  );
+server.on("request", (req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("pages/home.html", "utf-8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("pages/about.html", "utf-8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
+  }
 });
+console.log(`sever is running on: http://localhost:/${PORT}`);
 
-server.listen(3600);
+server.listen(PORT);
---
+++ someFiles/circle.js
@@ -0,0 +1,21 @@
+class Circle {
+  area(r) {
+    return Math.PI * r * r;
+  }
+  circumference(r) {
+    return 2 * Math.PI * r;
+  }
+}
+
+module.exports = Circle;
+
+// global objecjs
+
+//Globally available objects
+/*
+exports
+require
+mudle
+__dirName
+__fileName
+*/
